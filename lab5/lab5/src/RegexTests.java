7ec60259180cd0c7006403d00e1ad53c7a8976e4
BloodyOcean <bataricet@gmail.com>
2022-11-08 19:27:12 +0200

    5 lab

---
+++ lab5/lab5/src/RegexTests.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RegexTests {
+    @Test
+    void firstRegexTest() throws FileNotFoundException {
+        File text = new File("text.txt");
+        var sb = new StringBuilder();
+        Scanner scnr = new Scanner(text);
+
+        while(scnr.hasNextLine()){
+            String line = scnr.nextLine();
+            sb.append(line);
+        }
+
+        var res = StringHelper.start(sb.toString());
+        assertTrue(res.equals("hello world my name is edik _ petro i am _ software engineer _ from _ epam system half _ houra _ _ _ _ _ _ _"));
+    }
+
+    @Test
+    void secondRegexTest() throws FileNotFoundException {
+        String str = "an an an a";
+        var res = StringHelper.start(str);
+        assertTrue(res.equals("_ _ _ _"));
+    }
+
+    @Test
+    void thirdRegexTest() throws FileNotFoundException {
+        String str = "an inan an ina";
+        var res = StringHelper.start(str);
+        assertTrue(res.equals("_ inan _ ina"));
+    }
+
+    @Test
+    void fourthRegexTest() throws FileNotFoundException {
+        String str = "(an inan an ina)";
+        var res = StringHelper.start(str);
+        System.out.println(res);
+        assertTrue(res.equals("(_ inan _ ina)"));
+    }
+}
--- lab7/lab7/src/MatrixReader.java
+++ lab7/lab7/src/MatrixReader.java
@@ -1,2 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MatrixReader {
+    public static ArrayList<ArrayList<Double>> readMatrix(String path) throws FileNotFoundException {
+        // read in the data
+        ArrayList<ArrayList<Double>> a = new ArrayList<ArrayList<Double>>();
+        Scanner input = new Scanner(new File(path));
+        while(input.hasNextLine())
+        {
+            Scanner colReader = new Scanner(input.nextLine());
+            ArrayList col = new ArrayList();
+            while(colReader.hasNextDouble())
+            {
+                col.add(colReader.nextDouble());
+            }
+            a.add(col);
+        }
+
+        return a;
+    }
 }
